80748c19f69bd8f40ddc10cfbb63d787dad44af8
YASH7058 <yskssk16@gmail.com>
2026-08-29 12:55:05 +0530

    This is the day one of coding

---
+++ ay.java
@@ -0,0 +1,13 @@
+public class ay{
+    public static void main(String[] args) {
+        String  name = "Yash";
+        int age = 24;
+        String City = "Nilanga";
+        String Goal = "Software Engineer";
+
+        System.out.println("Name:"+name);
+        System.out.println("Age:"+age);
+        System.out.println("City:"+City);
+        System.out.println("Goal:"+Goal);
+    }
+}
--- practise.java
+++ practise.java
@@ -1,7 +1,18 @@
+import java.nio.channels.Pipe.SourceChannel;
+
 public class practise {
 
     public static void main(String[] args) {
-        System.out.println("hii this is yash");
+        
+        System.out.println("******************");
+        System.out.println("*                *");
+        System.out.println("*                *");
+        System.out.println("*      Java      *");
+        System.out.println("*                *");
+        System.out.println("*                *");
+        System.out.println("******************");
 
+
+        System.out.println("Java\nJava\nJava\nJava\nJava\nJava");
     }
 }
